576c4dea8e0bde6c1f0a2dfa6e3f3013d338c23a
Takinur M <takinurm@gmail.com>
2023-01-10 16:54:21 +0600

    Added Additional Page

---
+++ src/pages/wraith/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function index() {
+  return (
+    <div className=" flex h-screen items-center justify-center ">
+      <div className=" text-5xl font-extrabold">
+        <span className="bg-gradient-to-r from-green-500 to-violet-500 bg-clip-text text-transparent">
+          This one probably the Wraith Page. I am not sure.
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default index;

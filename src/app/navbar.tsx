e966638b7f91ad410b92eb10ab11d9427b1685ad
Takinur M <takinurm@gmail.com>
2023-02-07 11:28:32 +0600

    Client Side Component

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,21 +1,20 @@
+"use client"; // This is a client-side only file (no server-side rendering)
 import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <ul className="justify-between text-green-500">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/user/login">Login</Link>
-        </li>
-        <li>
-          <Link href="/user/signup">Sign Up</Link>
-        </li>
-      </ul>
+  return (
+    <ul className="justify-between text-green-500">
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/user/login">Login</Link>
+      </li>
+      <li>
+        <Link href="/user/signup">Sign Up</Link>
+      </li>
+    </ul>
+  );
+};
 
-
-    )
-}
-
-export default Navbar
+export default Navbar;

455785d49c768f9d13b389040c93750667eb07b1
Takinur M <takinurm@gmail.com>
2023-02-07 11:17:26 +0600

    Added Layout Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html>
+    <html lang='en'>
       <head />
       <body>{children}</body>
     </html>
